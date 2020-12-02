356bb03776784075509913dec7808f7488d2c4ba
Martin Puppe <dev@mpuppe.de>
2020-12-02 17:51:43 +0100

    Add GPL as license

--- shell.nix
+++ shell.nix
@@ -1,3 +1,22 @@
+/*
+    Copyright 2020 Martin Puppe
+
+    This file is part of Secrets.
+
+    Secrets is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Secrets is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Secrets.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
